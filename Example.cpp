ea32edc3f2f371c4c94e01065a9ca0e5ab05520d
Janna Shaftan <janna@Jannas-iMac.local>
2017-07-08 14:58:37 -0700

    First commit w/o test

---
+++ Example.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  I2C-interface
+//
+//  Created by JS on 7/8/17.
+//  Copyright © 2017 JS. All rights reserved.
+//
+
+#include <iostream>
+#include <iostream>
+#include <sstream>
+#include "bus/I2CDevice.h"
+
+using namespace std;
+using namespace exploringBB;
+int main(int argc, const char * argv[]) {
+    // test code
+}
